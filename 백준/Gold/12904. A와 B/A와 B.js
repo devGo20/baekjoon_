4e08293920c2377ec077d488fd91758f39df97b1
devGO <63961781+devGo20@users.noreply.github.com>
2025-03-31 17:19:28 +0900

    [Gold V] Title: A와 B, Time: 112 ms, Memory: 9364 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.js
+++ 백준/Gold/12904. A와 B/A와 B.js
@@ -1,18 +1,14 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 const S = input[0];
-let T = input[1];
+const T = input[1].split('');
 
-function canTransform() {
-  while (T.length > S.length) {
-    if (T[T.length - 1] === 'A') {
-      T = T.slice(0, -1);
-    } else if (T[T.length - 1] === 'B') {
-      T = T.slice(0, -1);
-      T = T.split('').reverse().join('');
-    }
+while (T.length > S.length) {
+  if (T.at(-1) === 'A') {
+    T.pop();
+  } else if(T.at(-1) ==='B'){
+    T.pop();
+    T.reverse();
   }
-  return S === T;
 }
-
-console.log(canTransform() ? 1 : 0);
+console.log(S === T.join('') ? 1 : 0);
