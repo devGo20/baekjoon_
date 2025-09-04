d97f1f68ec2a8ab092e5af33131cbfeedd3a9c4d
devGO <63961781+devGo20@users.noreply.github.com>
2025-09-04 20:13:19 +0900

    [Bronze V] Title: 사칙연산, Time: 96 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,7 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split(' ').map(Number);
+console.log(input[0] + input[1]);
+console.log(input[0] - input[1]);
+console.log(input[0] * input[1]);
+console.log(parseInt(input[0] / input[1]));
+console.log(parseInt(input[0] % input[1]));
+
