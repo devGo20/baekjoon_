c6245b49654662a1582885c281d9461e372ea3d3
devGO <63961781+devGo20@users.noreply.github.com>
2025-09-09 19:07:20 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 100 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.js
@@ -0,0 +1,5 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split(' ').map(Number);
+const currentYear = 2025;
+const thYear = 2562;
+const plus = thYear - currentYear;
+console.log(input - 543)
