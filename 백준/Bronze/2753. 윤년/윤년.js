006db7e5d8fc615f9ba914f33e6fb6c9f7190266
devGO <63961781+devGo20@users.noreply.github.com>
2025-09-13 17:12:35 +0900

    [Bronze V] Title: 윤년, Time: 108 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,7 @@
+// 윤년은 연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때이다.
+const input = Number(require('fs').readFileSync('/dev/stdin'))
+if ((input % 4 === 0 && input % 100 !== 0) || input % 400 === 0){
+    console.log(1)
+} else {
+    console.log(0)
+}
