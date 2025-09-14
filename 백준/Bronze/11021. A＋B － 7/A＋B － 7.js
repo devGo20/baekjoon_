5cfbd025117157e3254aaa894063d22f4a201ae3
devGO <63961781+devGo20@users.noreply.github.com>
2025-09-14 13:15:48 +0900

    [Bronze V] Title: A+B - 7, Time: 92 ms, Memory: 9364 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = process.platform === "linux" ? "/dev/stdin" : "./i.txt";
+
+const input = fs.readFileSync(path, "utf8").trim().split('\n')
+const n = input[0];
+const cases = input.slice(1);
+let answer = ''
+cases.forEach((item, idx) => {
+  const [a, b] = item.split(' ').map(Number)
+  answer += `Case #${idx+1}: ${a+b}\n`
+})
+process.stdout.write(answer)
