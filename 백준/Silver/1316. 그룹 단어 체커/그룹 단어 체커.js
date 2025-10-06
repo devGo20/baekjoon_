9b539974f6a70463d2867c8a1f1fb95c5c14ca65
devGO <63961781+devGo20@users.noreply.github.com>
2025-10-06 23:05:32 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 92 ms, Memory: 9436 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
@@ -1,26 +1,29 @@
 const fs = require("fs");
 const path = process.platform === "linux" ? "/dev/stdin" : "./i.txt";
 
-const input = fs.readFileSync(path, "utf8").trim().split('\n')
-const n = input[0]
-const strings = input.slice(1)
-let answer = 0
-for (const str of strings) {
-  const stack = [str[0]]
-  let correct = true
-  for (let i = 1; i < str.length; i++) {
-    if (stack.at(-1) !== str[i]) {
-      if (!stack.includes(str[i])) {
-        stack.push(str[i])
-      } else {
-        correct = false
-        break
-      }
-    }
-  }
-  if (correct) {
-    answer++
+const input = fs.readFileSync(path, "utf8").trim().split('\n');
+const n = Number(input[0]);
+const words = input.slice(1);
 
+let count = 0;
+
+for (const word of words) {
+  const seen = new Set();
+  let prev = "";
+  let isGroup = true;
+
+  for (const ch of word) {
+    if (ch !== prev) {
+      if (seen.has(ch)) {
+        isGroup = false;
+        break;
+      }
+      seen.add(ch);
+    }
+    prev = ch;
   }
+
+  if (isGroup) count++;
 }
-console.log(answer)
+
+console.log(count);
