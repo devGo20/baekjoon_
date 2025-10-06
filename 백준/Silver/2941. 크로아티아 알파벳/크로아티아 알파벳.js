536ce08636e916773b53088e9ed913332de1e4c8
devGO <63961781+devGo20@users.noreply.github.com>
2025-10-06 22:36:22 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 96 ms, Memory: 9360 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
@@ -2,10 +2,5 @@ const fs = require("fs");
 const path = process.platform === "linux" ? "/dev/stdin" : "./i.txt";
 
 let str = fs.readFileSync(path, "utf8").trim();
-const cro = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
-cro.forEach(c => {
-  if (str.includes(c)) {
-    str = str.replaceAll(c, '1')
-  }
-})
-console.log(str.length)
+const result = str.replace(/c=|c-|dz=|d-|lj|nj|s=|z=/g, 'X');
+console.log(result.length);
