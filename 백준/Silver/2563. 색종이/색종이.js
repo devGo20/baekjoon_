9c26ee531306715cd4afa7f6fd9ffada4019b589
devGO <63961781+devGo20@users.noreply.github.com>
2025-10-07 15:05:45 +0900

    [Silver V] Title: 색종이, Time: 96 ms, Memory: 9672 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.js
+++ 백준/Silver/2563. 색종이/색종이.js
@@ -7,22 +7,21 @@ const input = fs.readFileSync(path, "utf8").trim().split("\n").map(line =>
 
 const n = input[0][0];
 const coords = input.slice(1);
-const paper = Array(100).fill().map(() => Array(100).fill(false))
-coords.forEach((coord) => {
-  const [x, y] = coord
-  for (let i = y; i <= y + 9; i++) {
-    for (let j = x; j <= x + 9; j++) {
-      paper[i][j] = true
+const paper = Array(100).fill().map(() => Array(100).fill(false));
+
+coords.forEach(([x, y]) => {
+  for (let i = y; i < y + 10; i++) {
+    for (let j = x; j < x + 10; j++) {
+      paper[i][j] = true;
     }
   }
-})
+});
 
-let answer = 0
-paper.map((row, i) =>
-  row.map((r) => {
-    if (r) {
-      answer++
-    }
-  })
-)
-console.log(answer)
+let answer = 0;
+paper.forEach(row => {
+  row.forEach(cell => {
+    if (cell) answer++;
+  });
+});
+
+console.log(answer);
